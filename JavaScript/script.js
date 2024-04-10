5c98032d7186a19d17104e6cc6c8a5e77430a384
Gabriel Barn <gbrielbbarn@gmail.com>
2024-04-10 18:28:11 -0300

    [feat] element variables & favicon legal.
    numButtons variable holding numerals.
    functionButtons variable holding operators.
    calcScreen variable for the calculator display.

---
+++ JavaScript/script.js
@@ -0,0 +1,3 @@
+const numButtons = document.querySelectorAll('.button-num');
+const functionButtons = document.querySelectorAll('.button-function');
+const calcScreen = document.querySelector('#calc-screen');
