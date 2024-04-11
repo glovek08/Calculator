21f74a509b6d6473a746632750d21b7518f65927
Gabriel Barn <gbrielbbarn@gmail.com>
2024-04-10 23:12:07 -0300

    [feat] delay line description

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,7 +2,27 @@ const numButtons = document.querySelectorAll('.button-num');
 const functionButtons = document.querySelectorAll('.button-function');
 const calcScreen = document.querySelector('#calc-screen');
 
-let lastDisplayed = ['']; //This var is to store what was outputted last before changing. So we can recall to its content;
+let delayLine = {
+    delayLineA: [0],
+    delayLineB: [0],
+    current: 0,
+} /* In escence, when the user computes the first digit, it gets stored on delayLineA, second digit
+ on delayLineB, when an operation occurs, the output will be stored on current, pressing the clear key btn
+ will revert one step back, so it will look like this: 
+    current: [delayLineA[-1], delayLineB[-1]].
+ clearing all would revert both delayLineA and B to ['0'].
+
+ for example, pressing 1 as the first digit will become:
+    delayLine['delayLineA'].push(1);
+ then second digit:
+    delayLine['delayLineB'].push(5);
+ if at any point the user uses the recall button the current line will get reverted to the previous input, e.g.
+    -the user has already input 1 + 2 and then uses the recall button before pressing "="
+    delayLine.delayLineB.pop();
+    -now the current will be delayLineA +
+ then when the user computes:
+    delayLine.current = function(delayLine.delayLineA.pop(), delayLine.delayLineB.pop());
+ */ 
 
 functionButtons.forEach(el => {
     el.addEventListener('click', (functionButton) => {
@@ -18,13 +38,17 @@ functionButtons.forEach(el => {
 });
 numButtons.forEach(el => {
     el.addEventListener('click', (numButton) => {
-        console.log(numButton.target.id);
+        console.log(numButton.target.textContent);
+        displayOnScreen(numButton.target.textContent);
     })
 });
 
 const displayOnScreen = (contentToDisplay) => {
-    lastDisplayed.push(contentToDisplay);
-    calcScreen.textContent = contentToDisplay;
+    if (calcScreen.textContent === '0') {
+        calcScreen.textContent = contentToDisplay;
+        return;
+    }
+    calcScreen.textContent += contentToDisplay;
 }
 const clearScreen = () => {
     calcScreen.textContent = '0';
