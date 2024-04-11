556ff56a1fde6bcad6e865e49b398ed29ca78d7e
Gabriel Barn <gbrielbbarn@gmail.com>
2024-04-10 21:26:47 -0300

    [feat] clear screen and display content functions

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,31 @@
 const numButtons = document.querySelectorAll('.button-num');
 const functionButtons = document.querySelectorAll('.button-function');
 const calcScreen = document.querySelector('#calc-screen');
+
+let lastDisplayed = ['']; //This var is to store what was outputted last before changing. So we can recall to its content;
+
+functionButtons.forEach(el => {
+    el.addEventListener('click', (functionButton) => {
+        console.log(functionButton.target.id);
+        switch (functionButton.target.id) {
+            case 'clear-all-btn':
+                clearScreen();
+                break;
+            default:
+                displayOnScreen('GOOAAAL!!');
+        }
+    })
+});
+numButtons.forEach(el => {
+    el.addEventListener('click', (numButton) => {
+        console.log(numButton.target.id);
+    })
+});
+
+const displayOnScreen = (contentToDisplay) => {
+    lastDisplayed.push(contentToDisplay);
+    calcScreen.textContent = contentToDisplay;
+}
+const clearScreen = () => {
+    calcScreen.textContent = '0';
+};
