2a61a695c88d019c25e6ffea2a9819729f12db88
Gabriel Barn <gbrielbbarn@gmail.com>
2024-04-12 00:19:17 -0300

    [feat] display on screen, clear all, clear key.
    Display value of clicked numeral button on screen.
    Clicked element value gets stored on 'expression' var.
    firstInput boolean to display '0' if it's the first input on screen.
    
    -REFACTORING:
    'displayOnScreen' rewritten as 'refreshScreen'.
    'clearScreen' rewritten as 'clearAll'.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,55 +1,79 @@
+
+/*
+    User guide:
+    CA: Clear All, clears all entries and operations, essentially it resets the calculator.
+    CK: Clear Key, clear the last entry or the last digit entered on the display.
+    RC: Used to recall a stored value from memory.
+*/
+
+
 const numButtons = document.querySelectorAll('.button-num');
 const functionButtons = document.querySelectorAll('.button-function');
 const calcScreen = document.querySelector('#calc-screen');
 
-let delayLine = {
-    delayLineA: [0],
-    delayLineB: [0],
-    current: 0,
-} /* In escence, when the user computes the first digit, it gets stored on delayLineA, second digit
- on delayLineB, when an operation occurs, the output will be stored on current, pressing the clear key btn
- will revert one step back, so it will look like this: 
-    current: [delayLineA[-1], delayLineB[-1]].
- clearing all would revert both delayLineA and B to ['0'].
+let firstInput = true;
+let expression = '';
 
- for example, pressing 1 as the first digit will become:
-    delayLine['delayLineA'].push(1);
- then second digit:
-    delayLine['delayLineB'].push(5);
- if at any point the user uses the recall button the current line will get reverted to the previous input, e.g.
-    -the user has already input 1 + 2 and then uses the recall button before pressing "="
-    delayLine.delayLineB.pop();
-    -now the current will be delayLineA +
- then when the user computes:
-    delayLine.current = function(delayLine.delayLineA.pop(), delayLine.delayLineB.pop());
- */ 
+let mem = {
+    operand: [],
+    history: [],
+}
+
+/* 
+    the expression is stored in 'expression', when the user presses '='
+    the string is split into 'operand' using a regex that catches the symbols /+*-/n/,
+    using an if we call the operator function depending on which regex gets triggered. 
+    we could use a recurring function to check if the string contains each symbol. if it does,
+    it calculates the operation between the two closest operands and then call itself to continue
+    checking for symbol ocurrances.
+*/
 
 functionButtons.forEach(el => {
-    el.addEventListener('click', (functionButton) => {
-        console.log(functionButton.target.id);
-        switch (functionButton.target.id) {
+    el.addEventListener('click', (event) => {
+        console.log(event.target.id);
+        switch (event.target.id) {
             case 'clear-all-btn':
-                clearScreen();
+                if (calcScreen.textContent === '0') break;
+                clearAll();
+                break;
+            case 'clear-key-btn':
+                if (calcScreen.textContent === '0') break;
+                clearKey();
+                break;
+            case 'mult-btn':
+                break;
+            case 'result-btn':
                 break;
             default:
-                displayOnScreen('GOOAAAL!!');
+                refreshScreen('0');
         }
     })
 });
+
 numButtons.forEach(el => {
-    el.addEventListener('click', (numButton) => {
-        console.log(numButton.target.textContent);
-        displayOnScreen(numButton.target.textContent);
+    el.addEventListener('click', (event) => {
+        if (firstInput) calcScreen.textContent = '';
+        firstInput = false;
+        expression += event.target.value;
+        refreshScreen(expression);
     })
 });
 
-const displayOnScreen = (contentToDisplay) => {
-    if (calcScreen.textContent === '0') {
-        calcScreen.textContent = contentToDisplay;
-        return;
-    }
-    calcScreen.textContent += contentToDisplay;
+const clearKey = () => {
+    calcScreen.textContent = calcScreen.textContent.slice(0, -1);
+    expression = expression.slice(0, -1);
+    firstInput = true;
 }
-const clearScreen = () => {
+const multiply = (operand1, operand2) => {
+    return operand1 * operand2;
+}
+const refreshScreen = (contentToDisplay) => { /* Implement a way to always display 0*/
+    calcScreen.textContent = contentToDisplay;
+}
+const clearAll = () => {
+    firstInput = true;
+    expression = '';
+    mem.history = [];
+    mem.operand = [];
     calcScreen.textContent = '0';
 };
